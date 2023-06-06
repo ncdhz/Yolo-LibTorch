e9357b6ca655e516880baf2303540247b9f1f17e
ncdhz <1137436221@qq.com>
2023-06-06 14:22:50 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -9,13 +9,20 @@
 #define DEVICE "device"
 #define OUTPUT "output"
 #define ROI "roi"
+#define WIDTH "width"
+#define HEIGHT "height"
 #define PARA_NULL "null"
 #define PARA_TRUE "true"
+#define ERROR_WIDTH 30
+#define WINDOW_NAME "Yolo"
+#define OUTPUT_SUFFIX ".mp4"
 
 char PARAMETERS[][3][10] = {
 	{"-d", "--device", DEVICE},
 	{"-o", "--output", OUTPUT},
-	{"-r", "--roi", ROI}
+	{"-r", "--roi", ROI},
+	{"-x", "--width", WIDTH},
+	{"-y", "--height", HEIGHT}
 };
 
 int PARAMETER_LEN = sizeof(PARAMETERS) / sizeof(PARAMETERS[0]);
@@ -26,8 +33,10 @@ void help()
 	std::cout << "Options:" << std::endl;
 	std::cout << "-h    --help        understand the basic usage of software." << std::endl;
 	std::cout << "-d    --device      please enter the device being detected, which can be a file or a camera. The default parameter is 0, indicating camera number 0." << std::endl;
-	std::cout << "-o    --output      target detection data output path." << std::endl;
+	std::cout << "-o    --output      target detection data output file name." << std::endl;
 	std::cout << "-r    --roi         enable region of interest detection." << std::endl;
+	std::cout << "-x    --width       video display width." << std::endl;
+	std::cout << "-y    --height      video display height." << std::endl;
 	std::cout << std::endl;
 }
 
@@ -36,6 +45,8 @@ bool initParameters(std::map<std::string, std::string>& paras, int argc, char co
 	paras[DEVICE] = "0";
 	paras[OUTPUT] = PARA_NULL;
 	paras[ROI] = PARA_NULL;
+	paras[HEIGHT] = "640";
+	paras[WIDTH] = "640";
 
 	for (int i = 1; i < argc; i++)
 	{
@@ -74,13 +85,21 @@ bool initParameters(std::map<std::string, std::string>& paras, int argc, char co
 	return true;
 }
 
+void errorTemplate(std::string err)
+{
+	int center = (ERROR_WIDTH - err.length()) / 2 - 1;
+	std::cout << std::string(ERROR_WIDTH, '-') << std::endl;
+	std::cout << "|" << std::string(center, ' ');
+	std::cout << err;
+	std::cout << std::string(ERROR_WIDTH - err.length() - center - 2, ' ')  << "|" << std::endl;
+	std::cout << std::string(ERROR_WIDTH, '-') << std::endl;
+}
+
 bool parameterError()
 {
 	if (std::cin.fail())
-	{	
-		std::cout << "---------------------" << std::endl;
-		std::cout << "|  parameter error  |" << std::endl;
-		std::cout << "---------------------" << std::endl;
+	{
+		errorTemplate("parameter error");	
 		return true;
 	}
 	return false;
@@ -96,7 +115,7 @@ bool roiParameters(std::vector<cv::Point>& points)
 		return false;
 	}
 	if (num < 3) {
-		std::cout << "points must be greater than or equal to 3." << std::endl;
+		std::cerr << "points must be greater than or equal to 3." << std::endl;
 		return false;
 	}
 	for (size_t i = 1; i <= num; i++)
@@ -150,8 +169,20 @@ int main(int argc,  char const* argv[])
 	}
 	for (int j = 0; j < PARAMETER_LEN; j++) {
 		std::cout << std::left << std::setw(6) << PARAMETERS[j][0] << std::left << std::setw(15) << PARAMETERS[j][1] << paras[PARAMETERS[j][2]] << std::endl;
+	}	
+	// 初始化摄像头捕捉的范围，当输入为文件时可以忽略
+	int width = 0;
+	int height = 0;
+	if ((height = atoi(paras[HEIGHT].c_str())) == 0)
+	{
+		errorTemplate("height error"); 
+		return 0;
 	}
-	std::cout << std::endl;
+	if ((width = atoi(paras[WIDTH].c_str())) == 0)
+	{
+		errorTemplate("width error"); 
+		return 0;
+	}
 
 	bool existRoi = false;
 	
@@ -179,13 +210,25 @@ int main(int argc,  char const* argv[])
 		i++;
 	}
 	// 用 OpenCV 打开摄像头读取文件（你随便咋样获取图片都OK哪）
-	cv::VideoCapture cap = cv::VideoCapture(0);
+	cv::VideoCapture cap = cv::VideoCapture(paras[DEVICE]);
+	if (!cap.isOpened())
+	{
+		errorTemplate("device error");
+		return 0;
+	}
+	cv::namedWindow(WINDOW_NAME, cv::WINDOW_NORMAL | cv::WINDOW_FREERATIO);
+	cv::resizeWindow(WINDOW_NAME, width, height);
+	
 	// 设置宽高 无所谓多宽多高后面都会通过一个算法转换为固定宽高的
 	// 固定宽高值应该是你通过YoloV5训练得到的模型所需要的
 	// 传入方式是构造 YoloV5 对象时传入 width 默认值为 640，height 默认值为 640
-	cap.set(cv::CAP_PROP_FRAME_WIDTH, 1000);
-	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 800);
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
 	cv::Mat frame;
+
+	// 导出打标检测视频
+	cv::VideoWriter * outputVideo = nullptr;
+
 	while (cap.isOpened())
 	{
 		// 读取一帧
@@ -194,6 +237,11 @@ int main(int argc,  char const* argv[])
 		{
 			std::cout << "Read frame failed!" << std::endl;
 			break;
+		}
+		// 初始化输出方法
+		if (outputVideo == nullptr && strcmp(paras[OUTPUT].c_str(), PARA_NULL) != 0)
+		{
+			outputVideo = new cv::VideoWriter(paras[OUTPUT] + OUTPUT_SUFFIX, cv::VideoWriter::fourcc('M', 'P', '4', 'V'), cap.get(cv::CAP_PROP_FPS), frame.size());
 		}
 		// 预测
 		// 简单吧，两行代码预测结果就出来了，封装的还可以吧 嘚瑟
@@ -212,9 +260,23 @@ int main(int argc,  char const* argv[])
 
 		// 画框根据你自己的项目调用相应的方法，也可以不画框自己处理
 		frame = yolo.drawRectangle(frame, r[0], labels);
+		if (cv::waitKey(1) == 27 || cv::getWindowProperty(WINDOW_NAME, cv::WND_PROP_VISIBLE) < 1.0) break;
+
+		if (outputVideo != nullptr)
+		{
+			outputVideo->write(frame);
+		}
 		// show 图片
-		cv::imshow("", frame);
-		if (cv::waitKey(1) == 27) break;
+		cv::imshow(WINDOW_NAME, frame);
 	}
+
+	if (outputVideo != nullptr)
+	{
+		outputVideo->release();
+		delete outputVideo;
+	}
+
+	cap.release();
+	cv::destroyAllWindows();
 	return 0;
 }
